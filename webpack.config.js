e0fbcac32f70246e6ca678bcd36b62b2a98106fb
Laurence <laurence.nyein@gmail.com>
2019-01-20 15:05:04 +0000

    update webpack.config to load new main.ts instead of app.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,19 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './app/app.js',    
+    entry: './app/main.ts',
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+          }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
     output: {
         path: path.resolve(__dirname, 'app/dist'),
         filename: 'app.bundle.js',
